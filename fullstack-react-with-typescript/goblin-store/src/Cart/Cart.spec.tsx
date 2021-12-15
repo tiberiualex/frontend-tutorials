b3624f3d74bc0cb183dcc1965f9a657aa34f1a93
tiberiualex <tiberiu.alexander@gmail.com>
2021-12-15 22:52:09 +0200

    Test cart components

--- fullstack-react-with-typescript/goblin-store/src/Cart/Cart.spec.tsx
+++ fullstack-react-with-typescript/goblin-store/src/Cart/Cart.spec.tsx
@@ -1,99 +1,92 @@
-import React from "react"
-import { Cart } from "./Cart"
-import { fireEvent } from "@testing-library/react"
-import { CartItemProps } from "./CartItem"
+import React from "react";
+import { Cart } from "./Cart";
+import { fireEvent, render } from "@testing-library/react";
+import { CartItemProps } from "./CartItem";
+import renderWithRouter from "../testHelpers";
 
 jest.mock("./CartItem", () => ({
   CartItem: ({ product }: CartItemProps) => {
-    const { name, price, image } = product
+    const { name, price, image } = product;
+
     return (
       <div>
         {name} {price} {image}
       </div>
-    )
-  }
-}))
+    );
+  },
+}));
 
 describe("Cart", () => {
   describe("without products", () => {
-    const stubCartHook = () => ({
+    const cartHookMock = () => ({
       products: [],
       removeFromCart: () => {},
-      totalPrice: () => 0
-    })
+      totalPrice: () => 0,
+    });
 
     it("renders empty cart message", () => {
       const { container } = renderWithRouter(() => (
-        <Cart useCartHook={stubCartHook} />
-      ))
-      expect(container.innerHTML).toMatch(
-        "Your cart is empty."
-      )
-    })
+        <Cart useCartHook={cartHookMock} />
+      ));
 
-    describe("on 'Back to main page' click", () => {
+      expect(container.innerHTML).toMatch("Your cart is empty");
+    });
+
+    describe("on 'Back to main page'", () => {
       it("redirects to '/'", () => {
-        const {
-          getByText,
-          history
-        } = renderWithRouter(() => (
-          <Cart useCartHook={stubCartHook} />
-        ))
+        const { history, getByRole } = renderWithRouter(() => (
+          <Cart useCartHook={cartHookMock} />
+        ));
 
-        fireEvent.click(getByText("Back to main page."))
-
-        expect(history.location.pathname).toBe("/")
-      })
-    })
-  })
+        fireEvent.click(getByRole("link"));
+        expect(history.location.pathname).toBe("/");
+      });
+    });
+  });
 
   describe("with products", () => {
     const products = [
       {
         name: "Product foo",
         price: 100,
-        image: "/image/foo_source.png"
+        image: "/image/foo_source.png",
       },
       {
         name: "Product bar",
         price: 100,
-        image: "/image/bar_source.png"
-      }
-    ]
+        image: "/image/bar_source.png",
+      },
+    ];
 
-    const stubCartHook = () => ({
+    const cartHookStub = () => ({
       products,
       removeFromCart: () => {},
-      totalPrice: () => 55
-    })
+      totalPrice: () => 55,
+    });
 
     it("renders cart products list with total price", () => {
       const { container } = renderWithRouter(() => (
-        <Cart useCartHook={stubCartHook} />
-      ))
+        <Cart useCartHook={cartHookStub} />
+      ));
 
       expect(container.innerHTML).toMatch(
         "Product foo 100 /image/foo_source.png"
-      )
+      );
       expect(container.innerHTML).toMatch(
         "Product bar 100 /image/bar_source.png"
-      )
-      expect(container.innerHTML).toMatch("Total: 55 Zm")
-    })
+      );
+      expect(container.innerHTML).toMatch("Total: 55 Zm");
+    });
 
     describe("on 'go to checkout' click", () => {
       it("redirects to '/checkout'", () => {
-        const {
-          getByText,
-          history
-        } = renderWithRouter(() => (
-          <Cart useCartHook={stubCartHook} />
-        ))
+        const { history, getByText } = renderWithRouter(() => (
+          <Cart useCartHook={cartHookStub} />
+        ));
 
-        fireEvent.click(getByText("Go to checkout"))
-
-        expect(history.location.pathname).toBe("/checkout")
-      })
-    })
-  })
-})
+        fireEvent.click(getByText("Go to checkout"));
+        expect(history.location.pathname).toBe("/checkout");
+      });
+    });
+  });
+});
--- fullstack-react-with-typescript/goblin-store/src/Cart/CartItem.spec.tsx
+++ fullstack-react-with-typescript/goblin-store/src/Cart/CartItem.spec.tsx
@@ -1,48 +1,39 @@
-import React from "react"
-import { CartItem } from "./CartItem"
-import { Product } from "../shared/types"
-import { fireEvent } from "@testing-library/react"
+import React from "react";
+import { CartItem } from "./CartItem";
+import { Product } from "../shared/types";
+import { fireEvent } from "@testing-library/react";
+import renderWithRouter from "./../testHelpers";
 
 describe("CartItem", () => {
   const product: Product = {
     name: "Product Foo",
     price: 100,
-    image: "/image/source.png"
-  }
+    image: "/image/source.png",
+  };
 
   it("renders correctly", () => {
-    const {
-      container,
-      getByAltText
-    } = renderWithRouter(() => (
-      <CartItem
-        product={product}
-        removeFromCart={() => {}}
-      />
-    ))
+    const { container, getByAltText } = renderWithRouter(() => (
+      <CartItem product={product} removeFromCart={() => {}} />
+    ));
 
-    expect(container.innerHTML).toMatch("Product Foo")
-    expect(container.innerHTML).toMatch("100 Zm")
+    expect(container.innerHTML).toMatch("Product Foo");
+    expect(container.innerHTML).toMatch("100 Zm");
     expect(getByAltText("Product Foo")).toHaveAttribute(
       "src",
       "/image/source.png"
-    )
-  })
+    );
+  });
 
   describe("on 'Remove' click", () => {
     it("calls passed in function", () => {
-      const removeFromCartMock = jest.fn()
+      const spy = jest.fn();
 
       const { getByText } = renderWithRouter(() => (
-        <CartItem
-          product={product}
-          removeFromCart={removeFromCartMock}
-        />
-      ))
+        <CartItem product={product} removeFromCart={spy} />
+      ));
 
-      fireEvent.click(getByText("Remove"))
-
-      expect(removeFromCartMock).toBeCalledWith(product)
-    })
-  })
-})
+      fireEvent.click(getByText("Remove"));
+      expect(spy).toBeCalledWith(product);
+    });
+  });
+});
