0fbe92c0cf3648d77a7bd6c6ff467582f53a102c
tiberiualex <tiberiu.alexander@gmail.com>
2021-12-15 23:24:40 +0200

    Test checkout form

--- fullstack-react-with-typescript/goblin-store/src/Checkout/CheckoutForm.spec.tsx
+++ fullstack-react-with-typescript/goblin-store/src/Checkout/CheckoutForm.spec.tsx
@@ -1,65 +1,63 @@
-import React from "react"
-import { render, fireEvent } from "@testing-library/react"
-import { CheckoutForm } from "./CheckoutForm"
-import { act } from "react-dom/test-utils"
+import React from "react";
+import { render, fireEvent, getByLabelText } from "@testing-library/react";
+import { CheckoutForm } from "./CheckoutForm";
+import { act } from "react-dom/test-utils";
 
 describe("CheckoutForm", () => {
-  afterAll(jest.clearAllMocks)
   it("renders correctly", () => {
-    const { container } = render(<CheckoutForm />)
+    const { container } = render(<CheckoutForm />);
 
-    expect(container.innerHTML).toMatch("Cardholders Name")
-    expect(container.innerHTML).toMatch("Card Number")
-    expect(container.innerHTML).toMatch("Expiration Date")
-    expect(container.innerHTML).toMatch("CVV")
-  })
+    expect(container.innerHTML).toMatch("Cardholders Name");
+    expect(container.innerHTML).toMatch("Card Number");
+    expect(container.innerHTML).toMatch("Expiration Date");
+    expect(container.innerHTML).toMatch("CVV");
+  });
 
   describe("with invalid inputs", () => {
-    it("shows errors ", async () => {
-      const { container, getByText } = render(
-        <CheckoutForm />
-      )
+    it("shows errors", async () => {
+      const { container, getByText } = render(<CheckoutForm />);
 
       await act(async () => {
-        fireEvent.click(getByText("Place order"))
-      })
+        fireEvent.click(getByText("Place order"));
+      });
 
-      expect(container.innerHTML).toMatch("Error:")
-    })
-  })
+      expect(container.innerHTML).toMatch("Error:");
+    });
+  });
 
   describe("with valid inputs", () => {
     describe("on place order button click", () => {
       it("calls submit function with form data", async () => {
-        const mockSubmit = jest.fn()
+        const mockSubmit = jest.fn();
 
         const { getByLabelText, getByText } = render(
           <CheckoutForm submit={mockSubmit} />
-        )
+        );
 
         await act(async () => {
-          fireEvent.change(
-            getByLabelText("Cardholders Name:"),
-            { target: { value: "Bibo Bobbins" } }
-          )
+          fireEvent.change(getByLabelText("Cardholders Name:"), {
+            target: { value: "Bilbo Bobbins" },
+          });
+
           fireEvent.change(getByLabelText("Card Number:"), {
-            target: { value: "0000 0000 0000 0000" }
-          })
-          fireEvent.change(
-            getByLabelText("Expiration Date:"),
-            { target: { value: "3020-05" } }
-          )
+            target: { value: "0000 0000 0000 0000" },
+          });
+
+          fireEvent.change(getByLabelText("Expiration Date:"), {
+            target: { value: "3020-05" },
+          });
+
           fireEvent.change(getByLabelText("CVV:"), {
-            target: { value: "123" }
-          })
-        })
+            target: { value: "123" },
+          });
+        });
 
         await act(async () => {
-          fireEvent.click(getByText("Place order"))
-        })
+          fireEvent.click(getByText("Place order"));
+        });
 
-        expect(mockSubmit).toHaveBeenCalled()
-      })
-    })
-  })
-})
+        expect(mockSubmit).toHaveBeenCalled();
+      });
+    });
+  });
+});
